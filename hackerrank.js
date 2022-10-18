1d084af51eb5194dd40c712893f070d6f37dd120
JesacaLin <JesacaLin@gmail.com>
2022-10-17 23:18:49 -0400

    Day 3

--- hackerrank.js
+++ hackerrank.js
@@ -70,3 +70,83 @@ function solve(meal_cost, tip_percent, tax_percent) {
     console.log(Math.round(totalCost))
 
 }
+
+
+
+//LOOK - Day 3
+//Objective: In this challenge, we learn about conditional statements.
+//Task: Given an integer, , perform the following conditional actions:
+
+// If  is odd, print Weird
+// If  is even and in the inclusive range of  to , print Not Weird
+// If  is even and in the inclusive range of  to , print Weird
+// If  is even and greater than , print Not Weird
+// Complete the stub code provided in your editor to print whether or not  is weird.
+
+// SHORT SOLUTION
+function main() {
+    const N = parseInt(readLine().trim(), 10);
+
+if ((N % 2 !== 0) || (N >= 6 && N <= 20)){
+    console.log("Weird")
+} else{
+    console.log("Not Weird")
+}
+}
+
+// LONG SOLUTION
+function main() {
+    const N = parseInt(readLine().trim(), 10);
+
+if (N % 2 !== 0){
+    console.log("Weird")
+} if (N % 2 === 0){
+    if (N >= 2 && N <= 5){
+        console.log("Not Weird")
+    } else if (N >= 6 && N <= 20){
+        console.log("Weird")
+    } else{
+        console.log("Not Weird")
+    }
+}
+}
+
+
+
+
+
+//LOOK - Day 
+//Objective: 
+//Task: 
+
+
+
+
+//LOOK - Day 
+//Objective: 
+//Task: 
+
+
+
+//LOOK - Day 
+//Objective: 
+//Task: 
+
+
+
+//LOOK - Day 
+//Objective: 
+//Task: 
+
+
+
+
+//LOOK - Day 
+//Objective: 
+//Task: 
+
+
+
+//LOOK - Day 
+//Objective: 
+//Task: 
