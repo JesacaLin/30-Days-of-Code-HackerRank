91ad472a4e82d76d54dae88718570c89c8c7ff95
JesacaLin <JesacaLin@gmail.com>
2022-10-21 10:10:46 -0400

    Day 7

--- hackerrank.js
+++ hackerrank.js
@@ -8,13 +8,11 @@
 // You've got this!
 
 function processData(inputString) {
-    // This line of code prints the first line of output
-    console.log("Hello, World.");
-    console.log(inputString)
-    // Write the second line of output that prints the contents of 'inputString' here.
-    
+  // This line of code prints the first line of output
+  console.log("Hello, World.");
+  console.log(inputString);
+  // Write the second line of output that prints the contents of 'inputString' here.
 }
-
 
 //LOOK - Day 1
 // Task
@@ -27,24 +25,21 @@ function processData(inputString) {
 // Print the sum of  plus your double variable to a scale of one decimal place on a new line.
 // Concatenate  with the string you read as input and print the result on a new line.
 
-
-    // Declare second integer, double, and String variables.
+// Declare second integer, double, and String variables.
 let sumI;
 let rounded;
 let string;
-    // Read and save an integer, double, and String to your variables.
-sumI = Number(readLine()) //Alternatively, the unary operator can be used here: sumI = +(readLine())
-rounded = parseFloat(readLine()) //rounded = +(readLine())
-string = readLine() 
-    // Print the sum of both integer variables on a new line.
-console.log(i + sumI)
-    // Print the sum of the double variables on a new line.
+// Read and save an integer, double, and String to your variables.
+sumI = Number(readLine()); //Alternatively, the unary operator can be used here: sumI = +(readLine())
+rounded = parseFloat(readLine()); //rounded = +(readLine())
+string = readLine();
+// Print the sum of both integer variables on a new line.
+console.log(i + sumI);
+// Print the sum of the double variables on a new line.
 console.log((d + rounded).toFixed(1));
-    // Concatenate and print the String variables on a new line
-    // The 's' variable above should be printed first.
-console.log(s + string)
-
-
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+console.log(s + string);
 
 //LOOK - Day 2
 //Task
@@ -60,18 +55,15 @@ console.log(s + string)
  */
 
 function solve(meal_cost, tip_percent, tax_percent) {
-    // initialize variables to store calculated valuse of tip, tax and total cost
-    const tipAmount = (meal_cost / 100) * tip_percent;
-    const taxAmount = (tax_percent / 100 ) * meal_cost;
-    // total cost is the sum of the new variables
-    const totalCost = meal_cost + tipAmount + taxAmount
-    // round result to nearest integer
-    
-    console.log(Math.round(totalCost))
+  // initialize variables to store calculated valuse of tip, tax and total cost
+  const tipAmount = (meal_cost / 100) * tip_percent;
+  const taxAmount = (tax_percent / 100) * meal_cost;
+  // total cost is the sum of the new variables
+  const totalCost = meal_cost + tipAmount + taxAmount;
+  // round result to nearest integer
 
+  console.log(Math.round(totalCost));
 }
-
-
 
 //LOOK - Day 3
 //Objective: In this challenge, we learn about conditional statements.
@@ -85,35 +77,32 @@ function solve(meal_cost, tip_percent, tax_percent) {
 
 // SHORT SOLUTION
 function main() {
-    const N = parseInt(readLine().trim(), 10);
+  const N = parseInt(readLine().trim(), 10);
 
-if ((N % 2 !== 0) || (N >= 6 && N <= 20)){
-    console.log("Weird")
-} else{
-    console.log("Not Weird")
-}
+  if (N % 2 !== 0 || (N >= 6 && N <= 20)) {
+    console.log("Weird");
+  } else {
+    console.log("Not Weird");
+  }
 }
 
 // LONG SOLUTION
 function main() {
-    const N = parseInt(readLine().trim(), 10);
+  const N = parseInt(readLine().trim(), 10);
 
-if (N % 2 !== 0){
-    console.log("Weird")
-} if (N % 2 === 0){
-    if (N >= 2 && N <= 5){
-        console.log("Not Weird")
-    } else if (N >= 6 && N <= 20){
-        console.log("Weird")
-    } else{
-        console.log("Not Weird")
+  if (N % 2 !== 0) {
+    console.log("Weird");
+  }
+  if (N % 2 === 0) {
+    if (N >= 2 && N <= 5) {
+      console.log("Not Weird");
+    } else if (N >= 6 && N <= 20) {
+      console.log("Weird");
+    } else {
+      console.log("Not Weird");
     }
+  }
 }
-}
-
-
-
-
 
 //LOOK - Day 4
 //Objective: In this challenge, we're going to learn about the difference between a class and an instance; because this is an Object Oriented concept, it's only enabled in certain languages.
@@ -128,100 +117,172 @@ if (N % 2 !== 0){
 
 // Note: Do not remove or alter the stub code in the editor.
 
+function Person(initialAge) {
+  // Add some more code to run some checks on initialAge
 
+  if (Math.sign(initialAge) === -1) {
+    age = 0;
+    console.log("Age is not valid, setting age to 0.");
+  } else {
+    age = initialAge;
+  }
 
-function Person(initialAge){
-    // Add some more code to run some checks on initialAge
-
-    if (Math.sign(initialAge) === -1){
-        age = 0;
-        console.log("Age is not valid, setting age to 0.")
-    } else{
-        age = initialAge;
+  this.amIOld = function () {
+    // Do some computations in here and print out the correct statement to the console
+    if (age < 13) {
+      console.log("You are young.");
+    } else if (age < 18) {
+      console.log("You are a teenager.");
+    } else {
+      console.log("You are old.");
     }
-    
-  this.amIOld=function(){
-   // Do some computations in here and print out the correct statement to the console
-   if (age < 13){
-       console.log("You are young.")
-   } else if (age < 18){
-       console.log("You are a teenager.")
-   } else{
-       console.log("You are old.")
-   }
-
   };
-   this.yearPasses=function(){
-          // Increment the age of the person in here
-          age++;
-   };
+  this.yearPasses = function () {
+    // Increment the age of the person in here
+    age++;
+  };
 }
 
-
-
-
 //LOOK - Day 5
-//Objective: In this challenge, we will use loops to do some math. 
+//Objective: In this challenge, we will use loops to do some math.
 //Task: Given an integer, , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: n x i = result.
 
 function main() {
-    const n = parseInt(readLine().trim(), 10);
-    for (let i = 1; i <= 10; i++){
-        let answer = n * i
-        console.log(`${n} x ${i} = ${answer}`)
-    }
-};
-
-
-
-
-
+  const n = parseInt(readLine().trim(), 10);
+  for (let i = 1; i <= 10; i++) {
+    let answer = n * i;
+    console.log(`${n} x ${i} = ${answer}`);
+  }
+}
 
 //LOOK - Day 6
 //Objective: Today we will expand our knowledge of strings, combining it with what we have already learned about loops.
 //Task: Given a string, , of length  that is indexed from  to , print its even-indexed and odd-indexed characters as  space-separated strings on a single line (see the Sample below for more detail).
 
 function processData(input) {
-    //Enter your code here
-    const stringInput = input.split("\n")
+  //Enter your code here
+  const stringInput = input.split("\n");
 
-    let even = "";
-    let odd = "";
-   
-    
-    for (let i = 1; i < stringInput.length; i++){
-        for (let j =0; j < stringInput[i].length; j++){
-            if(j % 2 == 0){
-                even += stringInput[i][j]
-            } else {
-                odd += stringInput[i][j]
-            }
-        }
-        console.log(even + ' ' + odd);
-        even = "";
-        odd = "";
+  let even = "";
+  let odd = "";
+
+  for (let i = 1; i < stringInput.length; i++) {
+    for (let j = 0; j < stringInput[i].length; j++) {
+      if (j % 2 == 0) {
+        even += stringInput[i][j];
+      } else {
+        odd += stringInput[i][j];
+      }
     }
-} 
+    console.log(even + " " + odd);
+    even = "";
+    odd = "";
+  }
+}
 
+//LOOK - Day 7
+//Objective: Today, we will learn about the Array data structure.
+//Task: Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers.
 
+function main() {
+  const n = parseInt(readLine().trim(), 10);
 
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
 
+  console.log(arr.reverse().join(" "));
+}
 
+//LOOK - Day 8
+//Objective:
+//Task:
 
+//LOOK - Day
+//Objective:
+//Task:
 
-//LOOK - Day 
-//Objective: 
-//Task: 
+//LOOK - Day
+//Objective:
+//Task:
 
+//LOOK - Day
+//Objective:
+//Task:
 
+//LOOK - Day
+//Objective:
+//Task:
 
+//LOOK - Day
+//Objective:
+//Task:
 
-//LOOK - Day 
-//Objective: 
-//Task: 
+//LOOK - Day
+//Objective:
+//Task:
 
+//LOOK - Day
+//Objective:
+//Task:
 
+//LOOK - Day
+//Objective:
+//Task:
 
-//LOOK - Day 
-//Objective: 
-//Task: 
+//LOOK - Day
+//Objective:
+//Task:
+
+//LOOK - Day
+//Objective:
+//Task:
+
+//LOOK - Day
+//Objective:
+//Task:
+
+//LOOK - Day
+//Objective:
+//Task:
+
+//LOOK - Day
+//Objective:
+//Task:
+
+//LOOK - Day
+//Objective:
+//Task:
+
+//LOOK - Day
+//Objective:
+//Task:
+
+//LOOK - Day
+//Objective:
+//Task:
+
+//LOOK - Day
+//Objective:
+//Task:
+
+//LOOK - Day
+//Objective:
+//Task:
+
+//LOOK - Day
+//Objective:
+//Task:
+
+//LOOK - Day
+//Objective:
+//Task:
+
+//LOOK - Day
+//Objective:
+//Task:
+
+//LOOK - Day
+//Objective:
+//Task:
