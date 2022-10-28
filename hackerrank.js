1c6c4bed4fac93042123f7ee51d56c77dede3f52
JesacaLin <JesacaLin@gmail.com>
2022-10-27 20:40:53 -0400

    Day 11

--- hackerrank.js
+++ hackerrank.js
@@ -275,9 +275,38 @@ function main() {
   console.log(count);
 }
 
-//LOOK - Day 11 -
-//Objective:
-//Task:
+//LOOK - Day 11 - 2D ARRAYS
+//Objective: Today, we are building on our knowledge of arrays by adding another dimension.
+//Task: Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
+
+function main() {
+  let arr = Array(6);
+
+  for (let i = 0; i < 6; i++) {
+    arr[i] = readLine()
+      .replace(/\s+$/g, "")
+      .split(" ")
+      .map((arrTemp) => parseInt(arrTemp, 10));
+  }
+
+  //figuring out the minimum value: -9 is the given lowest constraints * 7, the numer in an hourglass.
+  let max = -63;
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      let sum = 0;
+      sum =
+        arr[i][j] +
+        arr[i][j + 1] +
+        arr[i][j + 2] +
+        arr[i + 1][j + 1] +
+        arr[i + 2][j] +
+        arr[i + 2][j + 1] +
+        arr[i + 2][j + 2];
+      max = max < sum ? sum : max;
+    }
+  }
+  console.log(max);
+}
 
 //LOOK - Day 12 -
 //Objective:
