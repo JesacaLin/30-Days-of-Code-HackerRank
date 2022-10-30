e832255c1f6ab92215b70a69c5496622ac2c8330
JesacaLin <JesacaLin@gmail.com>
2022-10-30 12:23:50 -0400

    Day 12

--- hackerrank.js
+++ hackerrank.js
@@ -308,9 +308,50 @@ function main() {
   console.log(max);
 }
 
-//LOOK - Day 12 -
-//Objective:
-//Task:
+//LOOK - Day 12 - INHERITANCE
+//Objective: Today, we're delving into Inheritance.
+//Task: You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
+
+class Student extends Person {
+  /*
+   *   Class Constructor
+   *
+   *   @param firstName - A string denoting the Person's first name.
+   *   @param lastName - A string denoting the Person's last name.
+   *   @param id - An integer denoting the Person's ID number.
+   *   @param scores - An array of integers denoting the Person's test scores.
+   */
+  // Write your constructor here
+  constructor(firstName, lastName, idNumber, scores) {
+    super();
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.idNumber = idNumber;
+    this.scores = scores;
+  }
+
+  /*
+   *   Method Name: calculate
+   *   @return A character denoting the grade.
+   */
+  // Write your method here
+  calculate() {
+    let average = this.scores.reduce((a, b) => a + b, 0) / this.scores.length;
+    if (average >= 90) {
+      return "O";
+    } else if (average >= 80) {
+      return "E";
+    } else if (average >= 70) {
+      return "A";
+    } else if (average >= 55) {
+      return "P";
+    } else if (average >= 40) {
+      return "D";
+    } else {
+      return "T";
+    }
+  }
+}
 
 //LOOK - Day
 //Objective:
