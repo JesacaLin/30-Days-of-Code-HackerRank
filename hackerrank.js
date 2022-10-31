50ffd3792d5246e1d696a91631475480a421feec
JesacaLin <JesacaLin@gmail.com>
2022-10-31 12:18:16 -0400

    Day 13

--- hackerrank.js
+++ hackerrank.js
@@ -353,13 +353,49 @@ class Student extends Person {
   }
 }
 
-//LOOK - Day
-//Objective:
-//Task:
+//LOOK - Day 13 - ABSTRACT CLASSES
+//Objective: Today, we will extend what we learned yesterday about Inheritance to Abstract Classes.
+//Task: Given a Book class and a Solution class, write a MyBook class that does the following:
 
-//LOOK - Day
-//Objective:
-//Task:
+// Inherits from Book
+// Has a parameterized constructor taking these  parameters:
+// string
+// string
+// int
+// Implements the Book class' abstract display() method so it prints these  lines:
+// , a space, and then the current instance's .
+// , a space, and then the current instance's .
+// , a space, and then the current instance's .
+
+// Declare your class here.
+/**
+ *   Class Constructor
+ *
+ *   @param title The book's title.
+ *   @param author The book's author.
+ *   @param price The book's price.
+ **/
+// Write your constructor here
+class MyBook extends Book {
+  constructor(title, author, price) {
+    super(title, author);
+    this.price = price;
+  }
+  /**
+   *   Method Name: display
+   *
+   *   Print the title, author, and price in the specified format.
+   **/
+  // Write your method here
+
+  display() {
+    console.log(`Title: ${this.title}`);
+    console.log(`Author: ${this.author}`);
+    console.log(`Price: ${this.price}`);
+  }
+}
+
+// End class
 
 //LOOK - Day
 //Objective:
